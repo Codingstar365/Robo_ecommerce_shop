ee13fce09c271f29ca8c45b77b6e74492616ae8e
ranja2025n <ranja2025n@gmail.com>
2025-06-28 18:45:56 +0530

    hjkj

--- src/routes/CollectionRoute.jsx
+++ src/routes/CollectionRoute.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import CollectionPages from '../pages/collectionPages';
-import CollectionPageSecond from '../pages/CollectionPageSecond';
+
 import CollectionPageThird from '../pages/CollectionPageThird';
 import UserOrder from '../pages/UserOrder';
+// import CollectionPages from '../pages/collectionPages';
+import CollectionPageSecond from '../pages/CollectionPageSecond';
 
 const CollectionRoutes = () => {
   return (
       <Routes >
-        <Route path="/allcategories" element={<CollectionPages/>}/> 
+        {/* <Route path="/allcategories" element={<CollectionPages/>}/>  */}
         <Route path="/shopbybrand" element={<CollectionPageSecond/>}/> 
         <Route path="/bulkorders" element={<CollectionPageThird/>}/> 
       </Routes>
